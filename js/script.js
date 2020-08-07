656898c194e7b2d5259a396844465e4fa158c85e
Magda Osik <magdalenaosik@gmail.com>
2020-08-07 07:32:31 +0200

    Add new files

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ const titleClickHandler = function (event) {
   }
 
   /* get 'href' attribute from the clicked link */
-  const articleSelektor = clickedElement.getAttribute('href');
+  const articleSelector = clickedElement.getAttribute('href');
   console.log('Link was clicked!')
   /* find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(articleSelector);
